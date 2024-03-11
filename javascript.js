20d7acf247400231ffe6d572508620c6f0907c93
selvy <selvyatef2004@gmail.com>
2024-03-11 03:46:28 +0200

    11/3/2024 -- 03:46AM

--- javascript.js
+++ javascript.js
@@ -1,34 +1,46 @@
 'use strict'
 
+// Images
 const decktopImage = document.querySelector('.desktop-phone');
 const phoneImage = document.querySelector('.photo-phone');
-
+// Icons
 const openIcon = document.querySelector(".open-icon");
 const closeIcon = document.querySelector(".close-icon");
+// Navigation
+const navigation = document.querySelector(".navigation");
 
-const navList = document.querySelector(".nav-list");
-
-if (window.innerWidth <= 380) {
+// Replace Desktop-img with Mobile-img
+if (window.innerWidth <= 500) {
   phoneImage.classList.remove('hidden');
   decktopImage.classList.add('hidden');
-
-  navList.classList.add("hidden");
-  openIcon.classList.remove("hidden");
-} else {
-  phoneImage.classList.add('hidden');
-  decktopImage.classList.remove('hidden');
-
-  navList.classList.remove("hidden");
-  openIcon.classList.add("hidden");
 }
 
+// Some Default for tablits and phones
+if (window.innerWidth <= 650) {
+  navigation.classList.add("opacity-nav");
+  openIcon.classList.remove("hidden");
+}
+
+// Open Navigation
 openIcon.addEventListener("click", function () {
   openIcon.classList.add("hidden");
-  closeIcon.classList.toggle("hidden");
-  navList.classList.remove("hidden");
+  closeIcon.classList.remove("hidden");
+  navigation.classList.remove("opacity-nav");
 });
-closeIcon.addEventListener("click", function () {
-  closeIcon.classList.toggle("hidden");
-  openIcon.classList.toggle("hidden");
-  navList.classList.add("hidden");
+
+// CLOSE NAVIGATION
+const closeNav = function () {
+  if (!closeIcon.classList.contains("hidden")) {
+    closeIcon.classList.toggle("hidden");
+    openIcon.classList.toggle("hidden");
+    navigation.classList.add("opacity-nav");
+  }
+}
+closeIcon.addEventListener("click", closeNav);
+
+//Close Navigation using Escape key on keyboard
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeNav();
+  }
 });
